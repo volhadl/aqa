a2cec299a91fe019c5cf8f55670e59a8c41429f5
Volha Dluhakanskaya <volha.dluhakanskaya@itechart-group.com>
2019-07-01 09:45:57 +0300

    9.45

--- Calc with TCs/Calculator_2.0/Calculator_2.0/Tests/BMITests.cs
+++ Calc with TCs/Calculator_2.0/Calculator_2.0/Tests/BMITests.cs
@@ -39,7 +39,8 @@ namespace Calculator_2._0.Tests
             Assert.AreEqual("BMI result : 55 / (1.7*2) = 19.03 \t It's Normal weight ", 
                 operation.calculation);
         }
-        
+       
+        /*
         [Test]
         public void BMIConsequenceTest()
         {
@@ -49,20 +50,25 @@ namespace Calculator_2._0.Tests
             Assert.AreEqual("Normal weight", a);
 
         }
+        */
         
-        /*
         [Test, TestCaseSource("ValidInputsAndOutputs")]
-        public void BMIConsequenceTestSec(double res, string expected)
+        public void BMIConsequenceTestSec(string res, string expected)
         {
             BaseOperation operation = new BMI();
-            string b = operation.BMIConsequence(res);
+            string b = operation.BMIConsequence(Convert.ToDouble(res));
             Assert.AreEqual(expected, b);
         }
 
         static object[] ValidInputsAndOutputs = new object[]
         {
-            new string[] {"19,03", "It's Normal weight"},
-            
-        };*/
+            new string[] { "18.5", "Underweight"},
+            new string[] { "18.51", "Normal weight"},
+            new string[] { "25", "Normal weight"},
+            new string[] { "26.01", "Overweight"},
+            new string[] { "30", "Overweight"},
+            new string[] { "30.01", "Obese"},
+
+        };
     }
 }
